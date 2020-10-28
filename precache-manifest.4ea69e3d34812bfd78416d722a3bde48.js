63123fba7d3ad4b27758f6c8a14c40a02e3a0746
elena-salam <elena.salamakha.ua@gmail.com>
2020-10-28 12:15:46 +0200

    Updates

---
+++ precache-manifest.4ea69e3d34812bfd78416d722a3bde48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b12274e98f301b3442a0ba085864ad",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c17714dcc499f98902dd",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "93f414d14d74056de280",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6b12c96f.chunk.css"
+  },
+  {
+    "revision": "c17714dcc499f98902dd",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b5a1b01e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b5a1b01e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93f414d14d74056de280",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.81ff6630.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
